d6745b18143cb161e07b6f412b3c4a76f9a94644
August Cornfeld <august.cornfeld@thelangschool.net>
2025-04-29 15:10:28 -0400

    FirstLine

--- Main.java
+++ Main.java
@@ -2,35 +2,41 @@
     import java.io.File;
     import java.io.FileReader;
     import java.io.IOException;
+    import java.util.ArrayList;
 
 public class Main {
 public static void main(String[] args){
     new Main();
 }
 String line= System.getProperty("line.separator");
-String strArray[] = new String[5];
+ArrayList<String> assignments = new ArrayList<>();
 
 
 
-public String readFirsln(String filename) throws IOException{
-                String content = null;
+public ArrayList<String> readFirstln(String filename){
                 File file = new File(filename); // For example, foo.txt
                 FileReader reader = null;
                 try {
                     String text = null;
                     BufferedReader brTest = new BufferedReader(new FileReader(file));
-                    text = brTest .readLine();
+                    text = brTest.readLine();
             System.out.println("text = " + text );
-             strArray= text.split(",");
+             String[] test=text.split(",");
+             int i=1;
+             for(String schoolWork: test){
+                if(i>=test.length){
+                    break;
+                }
+                assignments.add(test[i]);
+                i+=1;
+             }
                 } catch (IOException e) {
                     e.printStackTrace();
-                } finally {
-                    if(reader != null){
-                        reader.close();
-                    }
                 }
-                //System.out.println(content);
-                return content;
+                for(String a:assignments){
+                    System.out.print(a);
+                }
+                return assignments;
                 }
 /*public String readFile(String filename) throws IOException{
     String content = null;
@@ -53,6 +59,6 @@ public String readFirsln(String filename) throws IOException{
     return content;
     }*/
     public Main(){
-    readFirstln("Music");
+        readFirstln("data\\Music");
     }
 }
