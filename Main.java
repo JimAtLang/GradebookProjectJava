0715d9997c494976b9512135b75f941cb50f84fe
Milind Shah <milind.shah@thelangschool.net>
2025-04-30 14:01:41 -0400

    Created and Testing dropNumbers

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 public class Main {
+    
+    public static void main(String[] args){
 
+    }
+
+    public Main(){
+        CommonIndexHash commonIndexHash = new CommonIndexHash();
+        commonIndexHash.dropNumbers("HW1");
+    }
 }
