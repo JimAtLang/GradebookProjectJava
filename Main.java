bed831426639e5db6fb200385d3392f6490296ab
Jim Stewart <jim.stewart@thelangschool.org>
2025-05-13 14:23:31 -0400

    removed line that didn't work

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 public class Main {
-<<<<<<< HEAD
     
     public static void main(String[] args){
-
+        new Main();
     }
 
     public Main(){
+        CSV_reader csvr = new CSV_reader();
         CommonIndexHash commonIndexHash = new CommonIndexHash();
+        
     }
-=======
->>>>>>> CSV_textConvert
 }
