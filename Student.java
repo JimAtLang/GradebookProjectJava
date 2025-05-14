cc5d6654b68311c9081d38149c1dd9f51e9a6964
August Cornfeld <august.cornfeld@thelangschool.net>
2025-05-14 13:56:27 -0400

    fix pls

--- Student.java
+++ Student.java
@@ -1,3 +1,28 @@
-public class Student {
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+    private String name;
+    private HashMap<String, ArrayList<Integer>> Grades;
+    Student(String name,HashMap<String, ArrayList<Integer>> Grades){
+        this.name=name;
+        Grades= new HashMap<>();
+    }
+    // public void addGradesList(String type, ArrayList<Integer> values){
+    //     Grades.put(type, values);
+    // }
+    // public HashMap<String, ArrayList<Integer>> getGrades(){
+    //     return Grades;
+    // }
+    public void addGrade(String type, int grade, ArrayList<Integer> gradesList){
+        gradesList.add(grade);
+        Grades.replace(type, gradesList);
+    }
+    public void setName(){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
 }
