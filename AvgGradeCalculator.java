e7cbaaef532278eddaed758e8af978c867155b1e
Milind Shah <milind.shah@thelangschool.net>
2025-05-14 14:03:00 -0400

    Preparing to start AvgGradeCalculator

--- AvgGradeCalculator.java
+++ AvgGradeCalculator.java
@@ -1,3 +1,5 @@
-public class AvgGradeCalculator {
+class AvgGradeCalculator {
+    //Need two parts in the function:
+    //1. Grade-point convertor (for differently weighted assignments to gain a common denominator)
+    //2. Grade Averager (which averages the grades)
 
-}
