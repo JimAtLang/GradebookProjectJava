6f80f43c698859bf7fb5ef500c7ed2d9aac55f61
August Cornfeld <august.cornfeld@thelangschool.net>
2025-05-16 11:18:45 -0400

    final push probably

--- Student.java
+++ Student.java
@@ -9,18 +9,18 @@ public class Student {
         this.name=name;
         Grades= new HashMap<>();
     }
-    // public void addGradesList(String type, ArrayList<Integer> values){
-    //     Grades.put(type, values);
-    // }
-    // public HashMap<String, ArrayList<Integer>> getGrades(){
-    //     return Grades;
-    // }
+    public void addGradesList(String type, ArrayList<Integer> values){
+        Grades.put(type, values);
+    }
+    public HashMap<String, ArrayList<Integer>> getGrades(){
+        return Grades;
+    }
     public void addGrade(String type, int grade, ArrayList<Integer> gradesList){
         gradesList.add(grade);
         Grades.replace(type, gradesList);
     }
-    public void setName(){
-        this.name=name;
+    public void setName(String s){
+        this.name=s;
     }
     public String getName(){
         return name;
